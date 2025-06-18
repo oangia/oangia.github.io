fcbea0acdff9f1caadaa7b0453d6a0028aad532a
oangia <hqnhatdn@gmail.com>
2025-06-18 13:37:42 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='oangia',
+    version='0.1',
+    install_requires=[
+        'pydub',
+        'gTTS',
+        'SpeechRecognition',
+        "moviepy==1.0.3",
+        "gspread",
+        "pandas"
+    ]
+)
