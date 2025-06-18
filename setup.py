db65ef86b1a45b96921a1c09e9f8da6ee4a2e1a4
oangia <hqnhatdn@gmail.com>
2025-06-18 13:40:18 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ setup(
     name='oangia',
     version='0.1',
     install_requires=[
+        'yt-dlp',
+        'webvtt-py',
         'pydub',
         'gTTS',
         'SpeechRecognition',
-        "moviepy==1.0.3",
-        "gspread",
-        "pandas"
+        'moviepy==1.0.3',
+        'gspread',
+        'pandas'
     ]
 )
