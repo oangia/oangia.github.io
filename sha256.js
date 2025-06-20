19889e7242e06ebb47310465669b314f11a8d859
oangia <hqnhatdn@gmail.com>
2025-06-20 15:34:00 +0700

    Delete sha256.js

--- sha256.js
+++
@@ -1,8 +0,0 @@
-async function sha256(input) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(input);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
-}
