33719e27f7cb908d77cea3ddba150b770f944815
oangia <hqnhatdn@gmail.com>
2025-06-20 15:19:10 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,29 @@
+async function sha256(input) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+function setCookie(name, value, days) {
+  const expires = new Date(Date.now() + days*864e5).toUTCString();
+  document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
+}
+
+function getCookie(name) {
+  return document.cookie.split('; ').find(row => row.startsWith(name + '='))?.split('=')[1];
+}
+document.getElementById("login").addEventListener("keydown", async e => {
+  if (e.key !== "Enter") return;
+
+  const password = e.target.value;
+  const private = await sha256(password.repeat(5));
+  const public = await sha256(private);
+  if (public == public_key) {
+    setCookie("auth", public, 30);
+    window.location.href = "index.html"
+  } else {
+    alert("Invalid password.");
+  }
+});
