4ec427bd6c2054c0060688198e9cb826b0c259a3
oangia <hqnhatdn@gmail.com>
2025-06-21 13:55:55 +0700

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,56 +1,75 @@
 // sha256
 // getCookie
 // setCookie(key, value, days)
-async function sha256(t){let e=new TextEncoder,n=e.encode(t),i=await crypto.subtle.digest("SHA-256",n),o=Array.from(new Uint8Array(i)),r=o.map(t=>t.toString(16).padStart(2,"0")).join("");return r}function setCookie(t,e,n){let i=new Date(Date.now()+864e5*n).toUTCString();document.cookie=`${t}=${encodeURIComponent(e)}; expires=${i}; path=/`}function getCookie(t){return document.cookie.split("; ").find(e=>e.startsWith(t+"="))?.split("=")[1]}
-async function generatePrivate(password) {
-  const private_key = await sha256(password.repeat(5));
-  return private_key;
+function setCookie(t,e,n){let i=new Date(Date.now()+864e5*n).toUTCString();document.cookie=`${t}=${encodeURIComponent(e)}; expires=${i}; path=/`}function getCookie(t){return document.cookie.split("; ").find(e=>e.startsWith(t+"="))?.split("=")[1]}
+class Crypto {
+  async sha256(t){let e=new TextEncoder,n=e.encode(t),i=await crypto.subtle.digest("SHA-256",n),o=Array.from(new Uint8Array(i)),r=o.map(t=>t.toString(16).padStart(2,"0")).join("");return r}
 }
-async function generatePublic(private_key) {
-  const public_key = await sha256(private_key);
-  return public_key;
-}
-async function checkPrivateKey(private_key) {
-  const public_key = await generatePublic(private_key);
-  const response = await fetch("/public/" + public_key);
-  if (!response.ok) {
-    return false;
+
+class Password {
+  constructor() {
+    this.crypto = new Crypto();
   }
-  return (await response.text()).trim() == 'true';
-}
-function loginFail() {
-  if (window.location.pathname != '/login.html') {
-    window.location.href = "login.html";
-  } else {
-    alert("Invalid password.");
+  async generatePrivate(password) {
+    return await this.crypto.sha256(password.repeat(5));
   }
-}
-function loginSuccess(private_key) {
-  setCookie("auth", private_key, 30);
-  document.body.style.display = "block";
-  if (window.location.pathname == '/login.html') {
-    window.location.href = "index.html";
+  async generatePublic(private_key) {
+    return await this.crypto.sha256(private_key);
+  }
+  async checkPrivateKey(private_key) {
+    const public_key = await this.generatePublic(private_key);
+    const response = await fetch("/public/" + public_key);
+    if (!response.ok) {
+      return false;
+    }
+    return (await response.text()).trim() == 'true';
   }
 }
-async function checkLogin(private_key) {
-  const checkValid = await checkPrivateKey(private_key);
-  if (checkValid) {
-    loginSuccess(private_key);
-  } else {
-    loginFail();
+
+class Login {
+  constructor() {
+    this.password = new Password();
+  }
+  async loginPassword(inputId) {
+    const pInput = document.getElementById(inputId);
+    if (pInput != undefined) {
+      pInput.addEventListener("keydown", async e => {
+        if (e.key !== "Enter") return;
+        const password = e.target.value;
+        const private = await this.password.generatePrivate(password);
+        await this.checkLogin(private);
+      });
+    }
+  }
+  async login() {
+    const saved = getCookie("auth");
+    await this.checkLogin(saved);
+  }
+  async checkLogin(private_key) {
+    const checkValid = await this.password.checkPrivateKey(private_key);
+    if (checkValid) {
+      this.success(private_key);
+    } else {
+      this.fail();
+    }
+  }
+  fail() {
+    if (window.location.pathname != '/login.html') {
+      window.location.href = "login.html";
+    } else {
+      alert("Invalid password.");
+    }
+  }
+  success(private_key) {
+    setCookie("auth", private_key, 30);
+    document.body.style.display = "block";
+    if (window.location.pathname == '/login.html') {
+      window.location.href = "index.html";
+    }
   }
 }
-const pInput = document.getElementById("login");
-if (pInput != undefined) {
-  pInput.addEventListener("keydown", async e => {
-    if (e.key !== "Enter") return;
-    const password = e.target.value;
-    const private = await generatePrivate(password);
-    await checkLogin(private);
-  });
-}
-(async () => {
-  const saved = getCookie("auth");
-  await checkLogin(saved);
-})();
+const login = new Login();
+login.loginPassword("login");
+login.login();
+
 
