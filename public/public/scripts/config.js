6e1004325bd243914a5b0b0f9fdb0fae8b3c0738
hqnhatdn <hqnhatdn@gmail.com>
2025-10-27 03:25:24 +0000

    Studio

---
+++ public/public/scripts/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBG6KDG8G95hAA_wv7oWMzf8X3OQ2dVTpw",
+    authDomain: "stock-d7e0d.firebaseapp.com",
+    databaseURL: "https://stock-d7e0d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "stock-d7e0d",
+    storageBucket: "stock-d7e0d.appspot.com",
+    messagingSenderId: "145417125002",
+    appId: "1:145417125002:web:3fe97a52dd76cb04b2c173",
+    measurementId: "G-H7BLJZ4MG5"
+};
