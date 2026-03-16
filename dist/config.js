240859bcf19e56325e4fbcc0062fa5a066c71339
oangia <hqnhatdn@gmail.com>
2026-03-16 07:42:30 +0000

    Update

---
+++ dist/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB0TxR5HpNJ8Ph7rnrHqXNMAmBWo1dw5Nw",
+  authDomain: "agent52.firebaseapp.com",
+  projectId: "agent52",
+  storageBucket: "agent52.firebasestorage.app",
+  messagingSenderId: "534394830199",
+  appId: "1:534394830199:web:521b810d19dbcfe9edb572"
+};
