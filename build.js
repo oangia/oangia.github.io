edbe27e97a82cbe5d955421ea8d427b5aa129fa0
oangia <hqnhatdn@gmail.com>
2026-03-16 08:06:57 +0000

    Update

--- build.js
+++ build.js
@@ -46,8 +46,8 @@ build([
     "src/CUrl.js",
     "src/form.js",
     "src/app.js"
-], "dist/bundle.min.js");
+], "dist/scripts/bundle.min.js");
 
 build([
     "src/firebase/firebase.js"
-], "dist/firebase.min.js");
+], "dist/scripts/firebase.min.js");
