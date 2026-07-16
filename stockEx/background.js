98cdb636a8b956845c6ea60e79cb049a8c182815
oangia <hqnhatdn@gmail.com>
2026-07-16 08:44:53 +0000

    Update

--- stockEx/background.js
+++ stockEx/background.js
@@ -1,38 +1,15 @@
-import { Cache, StockAPI, listen } from './utils/background.js';
+import { Cache, listen } from './utils/background.js';
+import { Firebase } from "./firebase/facade.js";
 
-import { initializeApp } from "./firebase/firebase-app.js";
-import { getFirestore, collection, getDocs, doc, setDoc } from "./firebase/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB0TxR5HpNJ8Ph7rnrHqXNMAmBWo1dw5Nw",
-    authDomain: "agent52.firebaseapp.com",
-    projectId: "agent52",
-    storageBucket: "agent52.firebasestorage.app",
-    messagingSenderId: "534394830199",
-    appId: "1:534394830199:web:521b810d19dbcfe9edb572",
-    measurementId: "G-J9RZWL9DZ5"
-  };
-
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-async function fetchAll() {
-  const colRef = collection(db, "stocks");
-  const snap = await getDocs(colRef);
-
-  const data = snap.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data()
-  }));
-  
-  return data;
-}
-
-async function upsertStock(db, id, data) {
-  const ref = doc(db, "stocks", id);
-  await setDoc(ref, data, { merge: true });
-}
+const firebase = new Firebase({
+  apiKey: "AIzaSyB0TxR5HpNJ8Ph7rnrHqXNMAmBWo1dw5Nw",
+  authDomain: "agent52.firebaseapp.com",
+  projectId: "agent52",
+  storageBucket: "agent52.firebasestorage.app",
+  messagingSenderId: "534394830199",
+  appId: "1:534394830199:web:521b810d19dbcfe9edb572",
+  measurementId: "G-J9RZWL9DZ5"
+});
 
 const cache = new Cache(7 * 24 * 60 * 60 * 1000);
 const api = new StockAPI();
@@ -45,7 +22,7 @@ async function getStocks({ force = false } = {}) {
     }
   }
 
-  const data = await fetchAll();
+  const data = await firebase.all("stocks");
   await cache.set("stocks", data);
 
   return { ok: true, data, cached: false };
@@ -63,7 +40,7 @@ browser.runtime.onMessage.addListener(async (msg) => {
         .catch(err => ({ ok: false, error: err.toString() }));
 
     case "SEND_STOCK":
-      const data = await upsertStock(db, msg.code, msg.payload);
+      const data = await firebase.update("stocks", msg.code, msg.payload, true, { merge: true });
       return { ok: true, data };
   }
 });
